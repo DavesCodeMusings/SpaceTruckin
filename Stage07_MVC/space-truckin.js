f5c6f31926d44cb4be73845a8567870edbba589a
Dave Horton <dhorton668@outlook.com>
2020-04-20 11:34:24 -0500

    Clean up.

--- Stage07_MVC/space-truckin.js
+++ Stage07_MVC/space-truckin.js
@@ -1,3 +1,32 @@
+/* General Purpose */
+
+/*
+  If the width of the screen is greater than 768px, assume that this is a
+  device with plenty of screen real estate and open up details elements by
+  adding an attribut of 'open=true'.
+*/
+function openDetailsOnWideScreens() {
+  let mediaQueryList = window.matchMedia('(min-width: 768px)');
+  if (mediaQueryList.matches) {
+    let detailsElements = document.getElementsByTagName('details');
+    for (let i=0; i<detailsElements.length; i++) {
+      detailsElements[i].setAttribute('open', true);
+    }
+  }
+}
+
+/* Pull images into local cache for faster transitions. */
+function preloadImages() {
+  var starfieldImg = new Image('images/starfield.png');
+  var earthImg = new Image('images/earth.png');
+  var lunaImg = new Image('images/luna.png');
+  var venusImg = new Image('images/venus.png');
+  var marsImg = new Image('images/mars.png');
+  var ceresImg = new Image('images/ceres.png');
+  var jupiterImg = new Image('images/jupiter.png');
+  var saturnImg = new Image('images/saturn.png');
+}
+
 /* Modeling our universe */
 
 /* The ship we're piloting */
@@ -11,7 +40,7 @@ var ship = {
 
 /* Oh, the places you will go! */
 const locations = {
-  0: { 'portCode': '', 'name': 'In Transit', 'image': 'starfield.png' },
+  0: { 'portCode': '---', 'name': 'In Transit', 'image': 'starfield.png' },
   1: { 'portCode': 'ETH', 'name': 'Shreveport Space Elevator, Earth', 'image': 'earth.png' },
   2: { 'portCode': 'LUN', 'name': 'New Shanghai FTZ, Luna', 'image': 'luna.png' },
   3: { 'portCode': 'VEN', 'name': 'BNARMA Municipal Docks, Venus', 'image': 'venus.png' },
@@ -37,35 +66,8 @@ var finances = {
 
 /* Controlling our universe */
 
-/*
-  If the width of the screen is greater than 768px, assume that this is a
-  device with plenty of screen real estate and open up details elements by
-  adding an attribut of 'open=true'.
-*/
-function openDetailsOnWideScreens() {
-  let mediaQueryList = window.matchMedia('(min-width: 768px)');
-  if (mediaQueryList.matches) {
-    let detailsElements = document.getElementsByTagName('details');
-    for (let i=0; i<detailsElements.length; i++) {
-      detailsElements[i].setAttribute('open', true);
-    }
-  }
-}
-
 /* Switch image on the view screen. */
-function setImage(imageFileName) {
+function changeLocation(location) {
   let viewscreen = document.getElementById('viewscreen');
-  viewscreen.src = 'images/' + imageFileName;
-}
-
-/* Pull images into local cache for faster transitions. */
-function preloadImages() {
-  var starfieldImg = new Image('images/starfield.png');
-  var earthImg = new Image('images/earth.png');
-  var lunaImg = new Image('images/luna.png');
-  var venusImg = new Image('images/venus.png');
-  var marsImg = new Image('images/mars.png');
-  var ceresImg = new Image('images/ceres.png');
-  var jupiterImg = new Image('images/jupiter.png');
-  var saturnImg = new Image('images/saturn.png');
+  viewscreen.src = 'images/' + locations[location].image;
 }
